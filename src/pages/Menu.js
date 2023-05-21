6e2967b8a1fb5e19b5ace13622a32016bb7438fd
Stellazen <stellazen.xd@gmail.com>
2023-05-21 09:18:32 -0300

    inicio Menu

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,5 +1,29 @@
+import Quadrado from "../components/Quadrado"
+import './Menu.css'
 function Menu() {
-    return <p>Menu</p>
+    return( 
+        <section className="bodyMenu">
+
+            <div className="optionsBreakfast"> 
+                <Quadrado item ='Café da Manhã' />
+                <Quadrado item='Café americano' valor='R$5'/>
+                <Quadrado item='Café com leite'/>
+                <Quadrado item='Sanduíche de presunto e queijo'/>
+                <Quadrado item='Suco de fruta natural'/> 
+            </div>
+            <div className="optionsLunch">
+                <Quadrado item='Almoço'/>
+                <Quadrado item='Hambúrguer simples'/>
+                <Quadrado item='Hambúrguer duplo'/>
+                <Quadrado item='Batata frita'/>
+                <Quadrado item='Anéis de cebola'/>
+                <Quadrado item='Água 500ml'/>
+                <Quadrado item='Água 750ml'/>
+                <Quadrado item='Bebida gaseificada 500ml'/>
+                <Quadrado item='Bebida gaseificada 750ml'/>
+            </div>
+        </section>
+    )
 }
 
 export default Menu
