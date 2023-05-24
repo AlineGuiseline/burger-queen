8702b8423ad5ecd6127781f816571511bd9b5f0b
Aline Guiseline <alineguiseline@gmail.com>
2023-05-24 19:25:39 -0300

    Continuação da função de login

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,9 @@ import styles from './Button.module.css';
 function Button(props) {
   return (
     <div>
-      <button className={styles.btnEstilo} type="submit" onClick={props.onClick}>{props.name}</button>
+      {/* <button className={styles.btnEstilo} type="submit"
+      onClick={props.onClick}>{props.name}</button> */}
+      <button className={styles.btnEstilo} type="submit">{props.name}</button>
     </div>
   );
 }
