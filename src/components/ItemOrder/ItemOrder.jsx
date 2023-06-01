78c363f4360812c1200cc0846ac296271246aae9
Stellazen <stellazen.xd@gmail.com>
2023-06-01 14:37:30 -0300

    prints no resumo de pedidos

--- src/components/ItemOrder/ItemOrder.jsx
+++ src/components/ItemOrder/ItemOrder.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
-import styles from './ItemOrder.module.css';
+// import styles from './ItemOrder.module.css';
+
+// function ItemOrder(props) {
+//   return (
+//     <div className={styles.container}>
+//       <p>{props.item}</p>
+//       <p>{props.price}</p>
+//       <p>{props.number}</p>
+//       <div className={styles.icones}>
+//         <button type="submit" className={styles.adicionar}>+</button>
+//         <button type="submit" className={styles.retirar}>-</button>
+//       </div>
+//     </div>
+//   );
+// }
 
 function ItemOrder(props) {
   return (
-    <div className={styles.container}>
-      <p>{props.item}</p>
-      <p>{props.price}</p>
-      <p>{props.number}</p>
-      <div className={styles.icones}>
-        <button type="submit" className={styles.adicionar}>+</button>
-        <button type="submit" className={styles.retirar}>-</button>
-      </div>
-    </div>
+    <section>
+      <ul>
+        {props.orderItem.map((item) => (
+          <li>
+            <h4>{item.name}</h4>
+            <p>{item.price}</p>
+            <button type="submit">-</button>
+            <p>1</p>
+            <button type="submit">+</button>
+          </li>
+        ))}
+      </ul>
+
+      <h3>Total: R$</h3>
+    </section>
   );
 }
 
