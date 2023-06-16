c85af5c66e8198219b7eccdf936d1331fc085aa1
Aline Guiseline <alineguiseline@gmail.com>
2023-06-16 14:08:45 -0300

    Criação das telas do admin

---
+++ src/pages/Admin/Products/Products.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import LogoutButton from '../../../components/LogoutButton/LogoutButton';
+import Button from '../../../components/Button/Button';
+
+function Products() {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    try {
+      navigate(-1);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <header>
+      <LogoutButton />
+      <Button onClick={handleBack}> Voltar </Button>
+    </header>
+  );
+}
+
+export default Products;
