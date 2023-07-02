0ea61f667df16eaaef023869f7c8c26e8346cb04
Stellazen <stellazen.xd@gmail.com>
2023-07-02 16:40:29 -0300

    borda ao clicar InfoBoxTitle

--- src/components/InfoBoxTitle/InfoBoxTitle.jsx
+++ src/components/InfoBoxTitle/InfoBoxTitle.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import { React, useState } from 'react';
 import styles from './InfoBoxTitle.module.css';
 
 function QuadradoTitulo(props) {
+  const [corAtiva, setCorAtiva] = useState(false);
+
+  const handleClick = () => {
+    setCorAtiva(true);
+    props.onClick();
+  };
+
+  const resetarCor = () => {
+    setCorAtiva(false);
+  };
+
+  const estiloBotao = corAtiva ? `${styles.nomeItemTitulo} ${styles.corAtiva}` : styles.nomeItemTitulo;
+
   return (
     <div>
-      <button type="submit" onClick={props.onClick} className={styles.nomeItemTitulo}>{props.item}</button>
+      <button
+        type="submit"
+        onClick={handleClick}
+        className={estiloBotao}
+        onBlur={resetarCor}
+      >
+        {props.item}
+      </button>
     </div>
   );
 }
+
+// function QuadradoTitulo(props) {
+//   return (
+//     <div>
+//       <button
+//         type="submit"
+//         onClick={props.onClick}
+//         className={styles.nomeItemTitulo}
+
+//       >
+//         {props.item}
+//       </button>
+//     </div>
+//   );
+// }
 
 export default QuadradoTitulo;
